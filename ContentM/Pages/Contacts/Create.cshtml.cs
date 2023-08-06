e1af2b5e7cd1fff26c0533a63dede0a68f142fdb
Sanjanamodi <135058277+Sanjanamodi@users.noreply.github.com>
2023-08-05 20:00:27 -0400

    Commit for IES Group-1

---
+++ ContentM/Pages/Contacts/Create.cshtml.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ContactManager.Models;
+using ContentM.Data;
+using ContactManager.Authorization;
+using ContactManager.Pages.Contacts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace ContentM.Pages.Contacts
+{
+    public class CreateModel : DI_BasePageModel
+    {
+        public CreateModel(
+            ApplicationDbContext context,
+            IAuthorizationService authorizationService,
+            UserManager<IdentityUser> userManager)
+            : base(context, authorizationService, userManager)
+        {
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Contact Contact { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            Contact.OwnerID = UserManager.GetUserId(User);
+
+            var isAuthorized = await AuthorizationService.AuthorizeAsync(
+                                                        User, Contact,
+                                                        ContactOperations.Create);
+            if (!isAuthorized.Succeeded)
+            {
+                return Forbid();
+            }
+
+            Context.Contact.Add(Contact);
+            await Context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
